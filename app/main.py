3e7cd02dea585b61c0fe250ce6ba4cdde4fd94da
Lennart Hesse <lennart.hesse1301@gmail.com>
2026-02-11 13:06:11 +0100

    Added SignSeeker functionality

--- app/main.py
+++ app/main.py
@@ -1,75 +1,31 @@
 import cv2
-from collections import deque
 
-import numpy as np
-
-from app.utils import MP_model, Video
-from app.utils import convert_frame_to_mp_image, build_row_mean_std
-from app.model.inference import load_model
-
-
-BUFFER_SIZE = 30
-MIN_PROBA = 0.8
-PREDICT_EVERY_N = 10
-N = 0
+from app.model.seeker import SignSeeker
 
 
 if __name__ == "__main__":
 
-    buffer = deque(maxlen=BUFFER_SIZE)
-    classification_model, label_encoder = load_model("app/model/2k_v1_model.txt", "app/model/2k_v1_labelEncoder.pkl")
+    with SignSeeker() as skr:
 
-    mp_model = MP_model("app/hand_landmarker.task", MP_model.RunningMode.LIVE_STREAM)
-    
-    cap = cv2.VideoCapture(0)
-    time = 0
+        cap = cv2.VideoCapture(0)
 
-    while cap.isOpened():
-        success, bgr_frame = cap.read()
-        if not success:
-            print("Ignoring empty camera frame")
-            continue
-
-        mp_image_rgb = convert_frame_to_mp_image(bgr_frame)
-        mp_model.landmarker.detect_async(mp_image_rgb, time)
-
-        result, frame = None, None
-
-        with mp_model.frame_lock:
-            if mp_model.latest_frame is not None:
-                result = mp_model.latest_result
-                frame = mp_model.latest_frame
-
-        if result is not None and frame is not None:
-            buffer.append(result)
-
-            if len(buffer) >= BUFFER_SIZE and N > PREDICT_EVERY_N:
-                N = 0
-                video = Video("", list(buffer), "")
-
-                row = build_row_mean_std(video)
-                
-                if classification_model is not None and label_encoder is not None:
-                    X = np.array(row[:-1]).reshape(1, -1)
-                    y_proba: np.ndarray = classification_model.predict(X) # type: ignore
-                    top_y_pred = np.argmax(y_proba, axis=1)[0]
-
-                    top_proba = y_proba[0, top_y_pred]
-                    top_label = label_encoder.inverse_transform([top_y_pred])[0]
-
-                    if top_proba > MIN_PROBA:
-                        print(f"Prediction: {top_label} ({top_proba})")
+        while cap.isOpened():
+            success, bgr_frame = cap.read()
             
-            annotated_image_bgr = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
+            if not success:
+                print("Ignoring empty camera frame")
+                continue
 
-            cv2.imshow("Webcam", annotated_image_bgr)
-            if cv2.waitKey(1) & 0xFF == 27:  # ESC to quit
+            label, prob = skr.infer(bgr_frame)
+
+            if label is not None:
+                print(f"Prediction: {label} ({prob})")       
+
+            if skr.latest_frame_bgr is not None:
+                cv2.imshow("Webcam", skr.latest_frame_bgr)
+
+            if cv2.waitKey(100) & 0xFF == 27:  # ESC to quit
                 break
-        
-        N += 1
-        time += 1
 
-    # hopefully prevent segmentation error by destroying the mp_model
-    del mp_model
-    cap.release()
-    cv2.destroyAllWindows()
+        cap.release()
+        cv2.destroyAllWindows()
--- app/model/seeker.py
+++ app/model/seeker.py
@@ -1,7 +1,7 @@
 """Module providing a high-level interface to the extraction and classification logic."""
 
 from collections import deque
-from typing import Union, Tuple
+from typing import Union, Tuple, List
 
 import numpy as np
 import cv2
@@ -12,59 +12,79 @@ from app.utils import convert_frame_to_mp_image, build_row_mean_std
 from app.model.utils import load_model
 
 
-BUFFER_SIZE = 50
-MIN_PROBA = 0.8
-PREDICT_EVERY_N = 2
-N = 1               # always initialize/reset self.n to 1
+BUFFER_SIZE = 30
+MIN_PROBA = 0.08
+PREDICT_EVERY_N = 5
 
 class SignSeeker:
     """
     An extraction and classification model providing functionality for obtaining sign predictions from video frames.
     """
     def __init__(self):
-        self.mp_model = MP_model("app/hand_landmarker.task", MP_model.RunningMode.LIVE_STREAM)
-        self.classification_model, self.label_encoder = load_model("app/model/2k_v1_model.txt", "app/model/2k_v1_labelEncoder.pkl")
-        self.buffer = deque(maxlen=BUFFER_SIZE)
-        self.time = 0
-        self.n = N
+        self._mp_model = MP_model("app/hand_landmarker.task", MP_model.RunningMode.LIVE_STREAM)
+        self._classification_model, self._label_encoder = load_model("app/model/2k_v1_model.txt", "app/model/2k_v1_labelEncoder.pkl")
+        self._buffer = deque(maxlen=BUFFER_SIZE)
+        self._time = 0
+        self._n = 0
 
         self.latest_frame_bgr = None
-    
-    def infer(self, bgr_frame: MatLike) -> Union[Tuple[str, str], None]:
-        """
-        Takes in a 
-        """
 
+    def __enter__(self):
+        return self
+
+    def __exit__(self, type, value, traceback):
+        del self._mp_model
+    
+    def batch_infer(self, bgr_frames: List[MatLike]) -> Union[Tuple[str, float], Tuple[None, None]]:
+        """
+        Take a batch of frames and run a prediction on just this data as the buffer.
+        
+        :param bgr_frames: Batch of frames to run the prediction on
+        :type bgr_frames: List[MatLike]
+        :return: Predicted label and associated probability or (None, None)
+        :rtype: Tuple[str, float] | Tuple[None, None]
+        """
+        return None, None
+
+    def infer(self, bgr_frame: MatLike) -> Union[Tuple[str, float], Tuple[None, None]]:
+        """
+        Add a new frame to the models buffer and run a prediction. The model uses the last few frames in its buffer to aggregate the data and run the prediction.
+        
+        :param bgr_frame: Input frame in BGR color to add to the buffer.
+        :type bgr_frame: MatLike
+        :return: Predicted label and associated probability or (None, None)
+        :rtype: Tuple[str, float] | Tuple[None, None]
+        """
         mp_image_rgb = convert_frame_to_mp_image(bgr_frame)
-        self.mp_model.landmarker.detect_async(mp_image_rgb, self.time)
-        self.time += 1
+        self._mp_model.landmarker.detect_async(mp_image_rgb, self._time)
+        self._time += 1
+        self._n += 1
 
         result, frame = None, None
 
-        with self.mp_model.frame_lock:
-            if self.mp_model.latest_frame is not None:
-                result = self.mp_model.latest_result
-                frame = self.mp_model.latest_frame
+        with self._mp_model.frame_lock:
+            if self._mp_model.latest_frame is not None and self._mp_model.latest_result is not None:
+                result = self._mp_model.latest_result
+                frame = self._mp_model.latest_frame
 
         if result is not None and frame is not None:
-            self.buffer.append(result)
+            self._buffer.append(result)
             self.latest_frame_bgr = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
 
-            if len(self.buffer) >= BUFFER_SIZE and self.n >= PREDICT_EVERY_N:
-                self.n = N
+            if len(self._buffer) >= BUFFER_SIZE and self._n >= PREDICT_EVERY_N:
+                self._n = 0
 
                 # create a video object with empty filename and label since they don't exist and build row from video
-                video = Video("", list(self.buffer), "") 
+                video = Video("", list(self._buffer), "") 
                 row = build_row_mean_std(video)
                 
                 X = np.array(row[:-1]).reshape(1, -1) # explain why this is here
-                probabilities: np.ndarray = self.classification_model.predict(X) # type: ignore
+                probabilities: np.ndarray = self._classification_model.predict(X) # type: ignore
                 top_prediction = np.argmax(probabilities, axis=1)[0]
-                top_label = self.label_encoder.inverse_transform([top_prediction])[0]
+                top_label = self._label_encoder.inverse_transform([top_prediction])[0]
                 top_probability = probabilities[0, top_prediction]
 
                 if top_probability > MIN_PROBA:
-                    print(f"Prediction: {top_label} ({top_probability})")
                     return top_label, top_probability
         
-        return None
+        return None, None
